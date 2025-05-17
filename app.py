0a05fc3469a53f55cad43f1af305792eef67fe13
Matheus A. Boava <51657690+maboava@users.noreply.github.com>
2025-05-17 10:38:50 +0000

    Correção para puxar valores corretos dos sites e adição de estrutura for

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.dolarhoje.com/"
-resp = requests.get(url, headers={
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
-})
-soup = BeautifulSoup(resp.text, "html.parser")
+valores = []
 
-dolar_compra = soup.select_one("input#nacional")["value"]
+def sites():
+    urls = [
+        {"url": "https://www.dolarhoje.com/", "reference": "nacional"},
+        {"url": "https://wise.com/br/currency-converter/dolar-hoje", "reference": "target-input"},
+        {"url": "https://www.melhorcambio.com/dolar-hoje", "reference": "comercial"},
+    ]
 
-print("Dólar COMPRA:", dolar_compra)
+    for n in urls:
+        resp = requests.get(n["url"], headers={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+        })
+
+        soup = BeautifulSoup(resp.text, "html.parser")
+        reference = n["reference"]
+        dolar_compra = soup.select_one(f"input#{reference}")["value"]
+
+        dolar_compra = float(str((dolar_compra.replace(",","."))))
+        valores.append(dolar_compra)
+
+sites()       
+print(valores)
